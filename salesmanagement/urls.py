12f0b56489b71cff824f381f9f4873297ed97d77
“Pawan” <preyanshginti@gmail.com>
2025-04-09 15:11:08 +0530

    new changes

--- salesmanagement/urls.py
+++ salesmanagement/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),  # <== api/ prefix added here
+    path('api/', include('businessdetails.urls')),
+    path('api/', include('clients.urls')),
 ]
