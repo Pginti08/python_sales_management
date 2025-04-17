847f90d572c18bf7981239c04ef827df1e6b33d2
MandeepSingh1 <mandeep.singh671@gmail.com>
2025-04-17 13:20:53 +0530

    fix database path

--- salesmanagement/settings.py
+++ salesmanagement/settings.py
@@ -88,9 +88,9 @@ WSGI_APPLICATION = 'salesmanagement.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'salesdb',
-        'USER': 'vyzion',
-        'PASSWORD': 'vyzion123',
+        'NAME': 'vyzionuser',
+        'USER': 'vyzionuser',
+        'PASSWORD': '3tSfE7VMUaQ4yFrL',
         # 'HOST': 'localhost',
         'PORT': '5432',
     }
