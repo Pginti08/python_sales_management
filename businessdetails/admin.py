203eae354cf496752b880807a8b517d035065b40
“Pawan” <preyanshginti@gmail.com>
2025-04-25 16:55:16 +0530

    admin team size

--- businessdetails/admin.py
+++ businessdetails/admin.py
@@ -1,10 +1,15 @@
 # common_country_module/admin.py
 
 from django.contrib import admin
-from .models import  BusinessDetail
+from .models import BusinessDetail, TeamSize
 
 
 @admin.register(BusinessDetail)
 class BusinessDetailAdmin(admin.ModelAdmin):
-    list_display = ('user', 'business_name', 'team_size', 'phone', 'country')
+    list_display = ('user', 'business_name', 'team_size', 'phone', 'country', 'gstin')
     search_fields = ('business_category', 'code')
+
+@admin.register(TeamSize)
+class TeamSizeAdmin(admin.ModelAdmin):
+    list_display = ('size', 'id')
+    search_fields = ('size', 'id')
