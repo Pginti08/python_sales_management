a945efe5024a27ef1134f3b163864d26aa31f0a5
vyzion-innovations <preyanshginti@gmail.com>
2025-04-28 15:39:18 +0530

    new fields added

--- salesmanagement/settings.py
+++ salesmanagement/settings.py
@@ -95,27 +95,27 @@ WSGI_APPLICATION = 'salesmanagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test_development',
-        'USER': 'dev',
-        'PASSWORD': 'dev',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'sales_management',
-#         'USER': 'postgres',
-#         'PASSWORD': '1234',
-#         'HOST': 'localhost',
+#         'NAME': 'test_development',
+#         'USER': 'dev',
+#         'PASSWORD': 'dev',
+#         'HOST': '127.0.0.1',
 #         'PORT': '5432',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'salesmanagement',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 # Password validation abssdd
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
