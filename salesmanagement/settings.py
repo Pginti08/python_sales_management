0e7fea58458af3b184d96e4317d8ced8a561c694
vyzion-innovations <preyanshginti@gmail.com>
2025-04-30 18:27:10 +0530

    setting.py file changed

--- salesmanagement/settings.py
+++ salesmanagement/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+ALLOWED_HOSTS = env.str("ALLOWED_HOSTS", default="127.0.0.1").split(",")
 
 
 # Application definition
