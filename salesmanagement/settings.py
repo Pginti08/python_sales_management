8bb842328cb7a77cb59e5ef5c4bd4e210e47c531
vyzion-innovations <preyanshginti@gmail.com>
2025-05-01 12:25:55 +0530

    setting.py file changes

--- salesmanagement/settings.py
+++ salesmanagement/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=False)
+DEBUG = env.bool("DEBUG", default=True)
 
 ALLOWED_HOSTS = [ "api.vyzioninnovations.com",
     "127.0.0.1",
