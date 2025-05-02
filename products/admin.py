3086569d64dfe03bcb61a50b7fe84f980ed8d560
vyzion-innovations <preyanshginti@gmail.com>
2025-05-02 18:12:04 +0530

    bank object fetch

--- products/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from products.models import Product
-
-
-@admin.register(Product)
-class InvoiceDetailAdmin(admin.ModelAdmin):
-    list_display = ('user', 'amount', 'igst', 'business')
-    search_fields = ('amount', 'code')
